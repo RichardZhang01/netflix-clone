ee57a1f32e24bbe922524d1ce994627b9afb63b7
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-02 18:58:16 -0500

    create navbar component and add base styles.

--- src/components/HomeScreen/index.jsx
+++ src/components/HomeScreen/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './HomeScreen.css'
+import Navbar from '../Navbar'
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
-      
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+import NetflixLogo from "../../assets/images/netflix-logo.png";
+import Avatar from "../../assets/images/avatar.png";
 
 function Navbar() {
+  const [isShown, setIsShown] = useState(false);
+
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      setIsShown(true);
+    } else {
+      setIsShown(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+    return () => window.removeEventListener("scroll", transitionNavbar);
+  }, []);
+
   return (
-    <div>Navbar</div>
-  )
+    <div className={`nav ${isShown && "nav__black"}`}>
+      <div className="nav__contents">
+        <img className="nav__logo" src={NetflixLogo} alt="Netflix logo" />
+
+        <img className="nav__avatar" src={Avatar} alt="User avatar" />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
