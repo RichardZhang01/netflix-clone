4cbdc1bd6161dc06bcb447e3395fbdf217221af9
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-02 18:29:53 -0500

    cleanup file structure. Add firebase config file. Create HomeScreen and Navbar component folders/files.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCHsUJZd-R9i73By99ooOAUbAeGqlaYKSI",
+  authDomain: "netflix-clone-e2d22.firebaseapp.com",
+  projectId: "netflix-clone-e2d22",
+  storageBucket: "netflix-clone-e2d22.appspot.com",
+  messagingSenderId: "871348408418",
+  appId: "1:871348408418:web:1ce45550c74ca8582d813b",
+};
