7c20814a32c2dbbc59e9648ad962b59a3dd7b01d
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-03 16:56:05 -0500

    create utils: axios and requests that help with movie data fetching from TMDB. add movie fetching to banner component. Move firebase.js to utils. update index.css with background color. Modify styling of banner component. add dynamic data to banner component using movie data fetched using TMDB API

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,36 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axios";
+import requests from "../../utils/requests";
 import "./Banner.css";
-import BannerImage from "../../assets/images/banner.jpg";
+
+// import BannerImage from "../../assets/images/banner.jpg";
+import { FaPlay } from "react-icons/fa";
+import { BsPlusLg } from "react-icons/bs";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchBannerMovie() {
+      const request = await axiosInstance.get(requests.fetchNetflixOriginals);
+
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+
+      return request;
+    }
+
+    fetchBannerMovie();
+  }, []);
+
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        // backgroundImage: `url(${BannerImage})`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundColor: "black",
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">{movie.name}</h1>
         <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
+          <button className="banner__button banner__play">
+            <FaPlay className="banner__icon" /> Play
+          </button>
+          <button className="banner__button banner__list">
+            <BsPlusLg className="banner__icon" /> My List
+          </button>
         </div>
         <h1 className="banner__description">
-          Movie DescriptionMovie Description Movie Description Movie
-          DescriptionMovie DescriptionMovie Description Movie DescriptionMovie
-          DescriptionMovie Description Movie Description Movie DescriptionMovie
-          Description Movie DescriptionMovie Description Movie DescriptionMovie
-          DescriptionMovie Description Movie DescriptionMovie Description Movie
-          Description Movie DescriptionMovie Description Movie DescriptionMovie
-          DescriptionMovie DescriptionMovie DescriptionMovie Description Movie
-          Description Movie DescriptionMovie Description Movie DescriptionMovie
-          Description Movie DescriptionMovie DescriptionMovie DescriptionMovie
-          DescriptionMovie Description Movie DescriptionMovie Description Movie
-          DescriptionMovie Description Movie Description Movie Description
+          {movie.overview}
         </h1>
       </div>
 
---
+++ src/utils/requests.js
@@ -0,0 +1,24 @@
+// import * as dotenv from "dotenv";
+
+// dotenv.config();
+
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&language=en-us`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchAdventureMovies: `/discover/movie?api_key=${API_KEY}&with_genres=12`,
+  fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchDramaMovies: `/discover/movie?api_key=${API_KEY}&with_genres=18`,
+  fetchFantasyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=14`,
+  fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  fetchSciFiMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+};
+
+export default requests;
