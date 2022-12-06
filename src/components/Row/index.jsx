16c8ee2145faecf7e8ee0d33b0aae8538f334aba
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-05 19:22:17 -0500

    fix top-rated link in requests.js. add preliminary stylings for row components. modify banner and banner stylings slightly.

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axios";
 import "./Row.css";
 
-function Row(props) {
-  return <div className="row"></div>;
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const request = await axiosInstance.get(fetchUrl);
+
+      setMovies(request.data?.results);
+      return request;
+    }
+
+    fetchMovies();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className={`row__posters ${isLargeRow ? "row__postersLarge" : ""}`}>
+        {movies.map((movie) => {
+          if (movie.backdrop_path) {
+            return (
+              <img
+                className={`row__poster ${
+                  isLargeRow ? "row__posterLarge" : ""
+                }`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={`${movie.name} poster`}
+              />
+            );
+          }
+
+          return <></>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
