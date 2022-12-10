d75e14e59e271cf927356be72117e3a75b2e7c26
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-10 17:10:55 -0500

    refactor login background into Hero component.

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero__gradient" />
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './Login.css'
 
+import Hero from '../../components/Hero'
+
 import NetflixLogo from "../../assets/images/netflix-logo.png";
 
 function Login() {
   return (
     <div className="login">
-      <div className="login__background">
+        <Hero />
+      {/* <div className="login__background"> */}
         <div className="login__header">
           <img className="login__logo" src={NetflixLogo} alt="Netflix logo" />
           <button className="login__button">Sign In</button>
         </div>
-        <div className="login__gradient" />
-      </div>
+        {/* <div className="login__gradient" /> */}
+      {/* </div> */}
 
       <div className="login__body">
         <>
