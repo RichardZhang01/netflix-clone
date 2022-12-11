ecae4af7d48d773d8b786128a0a200c8aa1af992
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-10 19:31:10 -0500

    refactor login body into HeroBody component.

---
+++ src/components/HeroBody/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeroBody.css";
+
+function HeroBody() {
+  return (
+    <div className="heroBody">
+      <h1>Unlimited movies, TV shows, and more.</h1>
+      <h2>Watch anywhere. Cancel anytime.</h2>
+    </div>
+  );
+}
+
+export default HeroBody;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,18 +3,14 @@ import './Login.css'
 
 import Hero from '../../components/Hero'
 import Header from '../../components/Header'
+import HeroBody from '../../components/HeroBody';
 
 function Login() {
   return (
     <div className="login">
         <Hero />
         <Header />
-
-      <div className="login__body">
-        <>
-          <h1>Unlimited movies, TV shows, and more.</h1>
-        </>
-      </div>
+        <HeroBody />
     </div>
   );
 }
