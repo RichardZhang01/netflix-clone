b911017677e32f6f1104a802910444cafb96eef4
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-10 20:14:17 -0500

    update styles. Add unstyled form to HeroBody

--- src/components/HeroBody/index.jsx
+++ src/components/HeroBody/index.jsx
@@ -4,8 +4,21 @@ import "./HeroBody.css";
 function HeroBody() {
   return (
     <div className="heroBody">
-      <h1>Unlimited movies, TV shows, and more.</h1>
-      <h2>Watch anywhere. Cancel anytime.</h2>
+      <div className="heroBody__container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+      </div>
+
+      <form>
+        <h3>
+          Ready to watch? Enter your email to create or restart your membership.
+        </h3>
+        <div>
+          <input type="email" />
+          <label htmlFor="">Email Address</label>
+        </div>
+        <button type="submit">Get Started</button>
+      </form>
     </div>
   );
 }
