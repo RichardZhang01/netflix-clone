1de16577be82a965f5c0748c6ccb4c1014228517
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-20 21:41:08 -0500

    update stylings. add email validation and error handling to EmailForm component.

--- src/components/EmailForm/index.jsx
+++ src/components/EmailForm/index.jsx
@@ -1,24 +1,46 @@
-import React from "react";
-import './EmailForm.css'
+import React, { useState } from "react";
+import "./EmailForm.css";
 
 import { BsChevronRight } from "react-icons/bs";
 
 function EmailForm() {
+  const [emailError, setEmailError] = useState(null);
+
+  const emailErrorHandler = (event) => {
+    const inputEmail = event.target.value.trim();
+    const re =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    
+    if (!inputEmail) return setEmailError('Email is required!')
+
+    if (!re.test(inputEmail)) return setEmailError('Please enter a valid email address')
+
+    setEmailError(null)
+  }
+
   return (
     <form className="emailForm">
-      <div className="emailForm__inputGroup">
-        <input type="text" id="emailForm__input" required="true" />
-        <label
-          htmlFor="emailForm__input"
-          className="emailForm__label"
-        >
-          Email Address
-        </label>
+      <h3 className="emailForm__title">
+        Ready to watch? Enter your email to create or restart your membership.
+      </h3>
+      <div className="emailForm__container">
+        <div className="emailForm__inputGroup">
+          <input
+            type="text"
+            id="emailForm__input"
+            onBlur={emailErrorHandler}
+            required={true}
+          />
+          <label htmlFor="emailForm__input" className="emailForm__label">
+            Email Address
+          </label>
+          <button type="submit" className="emailForm__button">
+            Get Started{" "}
+            <BsChevronRight className="emailForm__button__chevron" />
+          </button>
+        </div>
+        {emailError && <div className="emailForm__inputError__text">{emailError}</div>}
       </div>
-      <button type="submit" className="emailForm__button">
-        Get Started{" "}
-        <BsChevronRight className="emailForm__button__chevron" />
-      </button>
     </form>
   );
 }
--- src/components/HeroBody/index.jsx
+++ src/components/HeroBody/index.jsx
@@ -8,9 +8,6 @@ function HeroBody() {
       <div className="heroBody__container">
         <h1>Unlimited movies, TV shows, and more.</h1>
         <h2>Watch anywhere. Cancel anytime.</h2>
-        <h3>
-          Ready to watch? Enter your email to create or restart your membership.
-        </h3>
 
         <EmailForm />
       </div>
