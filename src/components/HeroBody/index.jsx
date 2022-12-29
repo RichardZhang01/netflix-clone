3d7547a835afb51055684c3c3ade29f39b739b31
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-29 15:38:28 -0500

    update login page to reflect changes on Netflix's landing page

--- src/components/HeroBody/index.jsx
+++ src/components/HeroBody/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+
 import "./HeroBody.css";
-import EmailForm from "../EmailForm";
+// import EmailForm from "../EmailForm";
+
+import { BsChevronRight } from "react-icons/bs";
 
 function HeroBody() {
   return (
     <div className="heroBody">
       <div className="heroBody__container">
-        <h1>Unlimited movies, TV shows, and more.</h1>
-        <h2>Watch anywhere. Cancel anytime.</h2>
+        <h1>Plans for every Budget.</h1>
+        <h2>Get Basic with ads for just $5.99.</h2>
 
-        <EmailForm />
+        <button className="heroBody__button">
+          Get Started <BsChevronRight className="heroBody__button__chevron" />
+        </button>
+        {/* <EmailForm /> */}
       </div>
     </div>
   );
