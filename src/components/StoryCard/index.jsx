9990185cd28bd6e5d2e5b9e6b061e27bde66a06f
RZhang <richardzhiyuanzhang@gmail.com>
2023-01-12 18:47:32 -0500

    create skeleton for StoryCard component

--- src/components/StoryCard/index.jsx
+++ src/components/StoryCard/index.jsx
@@ -2,10 +2,28 @@ import React from "react";
 import "./StoryCard.css";
 
 function StoryCard({ inverted = false }) {
+  function StoryCardText() {
+    return <div className="storyCard__text"></div>;
+  }
+
+  function StoryCardVideo() {
+    return <div className="storyCard__video"></div>;
+  }
+
   if (!inverted) {
-    return <div>StoryCard</div>;
+    return (
+      <div className="storyCard">
+        <StoryCardText />
+        <StoryCardVideo />
+      </div>
+    );
   } else {
-    return <div>Inverted StoryCard</div>;
+    return (
+      <div className="storyCard">
+        <StoryCardVideo />
+        <StoryCardText />
+      </div>
+    );
   }
 }
 
