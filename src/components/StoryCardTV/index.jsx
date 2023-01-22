47371d5fc318e7b313b624e7dbe178a67215615f
RZhang <richardzhiyuanzhang@gmail.com>
2023-01-21 20:04:36 -0500

    update StoryCardText component to use props

--- src/components/StoryCardTV/index.jsx
+++ src/components/StoryCardTV/index.jsx
@@ -7,23 +7,26 @@ import StoryCardText from "../StoryCardText";
 
 function StoryCardTV() {
   return (
-      <div className="storyCardTV">
-        <div className="storyCardTV__container">
-          <StoryCardText />
-          <div className="storyCardTV__videoContainer">
-            <img className="storyCardTV__image" src={TV} alt="TV" />
-            <video
-              src={Video}
-              className="storyCardTV__video"
-              autoPlay={true}
-              playsInline={true}
-              muted={true}
-              loop={true}
-            ></video>
-          </div>
+    <div className="storyCardTV">
+      <div className="storyCardTV__container">
+        <StoryCardText
+          headingOneText="Enjoy on your TV."
+          headingTwoText="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+        />
+        <div className="storyCardTV__videoContainer">
+          <img className="storyCardTV__image" src={TV} alt="TV" />
+          <video
+            src={Video}
+            className="storyCardTV__video"
+            autoPlay={true}
+            playsInline={true}
+            muted={true}
+            loop={true}
+          ></video>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default StoryCardTV;
--- src/components/StoryCardText/index.jsx
+++ src/components/StoryCardText/index.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import './StoryCardText.css'
+import React from "react";
+import "./StoryCardText.css";
 
-function StoryCardText() {
+function StoryCardText({ headingOneText, headingTwoText }) {
   return (
     <div className="storyCard__textContainer">
-      <h1 className="storyCard__title">Enjoy on your TV.</h1>
-      <h2 className="storyCard__subtitle">
-        Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
-        players, and more.
-      </h2>
+      <h1 className="storyCard__title">{headingOneText}</h1>
+      <h2 className="storyCard__subtitle">{headingTwoText}</h2>
     </div>
   );
 }
 
-export default StoryCardText
+export default StoryCardText;
