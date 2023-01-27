343e2b1dddb06edd510177fbf0549600dc9aa37d
RZhang <richardzhiyuanzhang@gmail.com>
2023-01-26 19:06:01 -0500

    update faq story card component with list items

--- src/components/StoryCardFAQ/index.jsx
+++ src/components/StoryCardFAQ/index.jsx
@@ -4,13 +4,22 @@ import './StoryCardFAQ.css'
 import EmailForm from '../EmailForm'
 
 function StoryCardFAQ() {
+
+  function FAQListItems() {
+    return (
+      <li></li>
+    )
+  }
+
   return (
     <div class="storyCardFAQ">
         <div className="storyCardFAQ__container">
             <h1 className="storyCardFAQ__heading">
                 Frequently Asked Questions
             </h1>
-            <ul className="storyCardFAQ__FAQContainer"></ul>
+            <ul className="storyCardFAQ__FAQContainer">
+              <FAQListItems />
+            </ul>
             <EmailForm />
         </div>
     </div>
