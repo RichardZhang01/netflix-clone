225a7210bf1e8706df5099ee14de782a5cc395dc
RZhang <richardzhiyuanzhang@gmail.com>
2023-02-04 17:50:56 -0500

    update firebase util

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./SignIn.css";
 
 import { useLocation } from "react-router-dom";
+import Hero from "../../components/Hero";
+import Header from "../../components/Header";
 
 function SignIn() {
   const location = useLocation();
   const email = location.state ? location.state.email : "";
+  console.log(email);
 
-  return <div className="signin">{email || "No email"}</div>;
+  return (
+    <div className="signin">
+      <Hero />
+      <Header />
+    </div>
+  );
 }
 
 export default SignIn;
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCHsUJZd-R9i73By99ooOAUbAeGqlaYKSI",
   authDomain: "netflix-clone-e2d22.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "871348408418",
   appId: "1:871348408418:web:1ce45550c74ca8582d813b",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
