77b706386f885fcd038db8bc3667d151588602ca
RZhang <richardzhiyuanzhang@gmail.com>
2023-02-05 20:56:51 -0500

    update signinform component with form styles

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import './SignInForm.css'
+import React from "react";
+import "./SignInForm.css";
 
 function SignInForm() {
   return (
-    <div className='signInForm__container'>
+    <div className="signInForm__container">
+      <h1 className="signInForm__heading">Sign In</h1>
+      <form className="signInForm">
+        <div className="signInForm__emailInputGroup">
+          <input id="signInForm__emailInput" type="text" required={true} />
+          <label
+            className="signInForm__emailLabel"
+            htmlFor="signInForm__emailInput"
+          >
+            Email or phone number
+          </label>
+        </div>
 
+        <div className="signInForm__passwordInputGroup">
+          <input
+            id="signInForm__passwordInput"
+            type="password"
+            required={true}
+          />
+          <label
+            className="signInForm__passwordLabel"
+            htmlFor="signInForm__passwordInput"
+          >
+            Password
+          </label>
+          {/* <button>SHOW</button> */}
+        </div>
+
+        <button type="submit" className="signInForm__signInButton">
+          Sign In
+        </button>
+
+        <div className="signInForm__rememberMeInputGroup">
+          <input id="signInForm__rememberMeInput" type="checkbox" />
+          <label
+            className="signInForm__rememberMeLabel"
+            htmlFor="signInForm__rememberMeInput"
+          >
+            Remember me
+          </label>
+        </div>
+      </form>
+      <div></div>
     </div>
-  )
+  );
 }
 
-export default SignInForm
+export default SignInForm;
